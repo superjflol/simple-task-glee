6b73712885903677dc97a493b05cf349bfd3ff9f
picealessandroo@gmail.com <picealessandroogmail.com>
2025-03-22 15:16:48 +0100

    greetings

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  // ...existing code...
-  site: 'https://judgment-fleet.netlify.app',
-  integrations: [
-    // ...existing code...
-  ]
-});
